006926e10c5bc99450e23ffca62a91799f692d45
apierce1992 <apierce1992@gmail.com>
2015-10-16 15:21:36 -0400

    added url redirect

---
+++ urly_bird/urly/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+
+
+urlpatterns = [
+        url(r'^(?P<click_short>\w+)$', views.short, name='redirect_shorts'),
+
+
+
+]
--- urly_bird/urly/views.py
+++ urly_bird/urly/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Click
+
 
 # Create your views here.
+def short(request, click_short):
+    click = Click.objects.get(short=click_short)
+    return redirect(click.orig)
