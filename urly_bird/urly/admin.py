815753359834c23efad90394b56b0f1bf9dc0562
apierce1992 <apierce1992@gmail.com>
2015-10-16 13:41:30 -0400

    admin

--- urly_bird/urly/admin.py
+++ urly_bird/urly/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Click
 
-# Register your models here.
+
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'timestamp', 'orig', 'short']
+
+
+
+admin.site.register(Click, ClickAdmin)
