66e99e90df5b9bbe541c51cc27fc81f0036eea06
Kathleen Rauh <krauh2010@gmail.com>
2015-10-16 16:22:37 -0400

    Beginning of pagination.

--- urly_bird/urly/views.py
+++ urly_bird/urly/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.core import Paginator, EmptyPage, PageNotAnInteger
+
+
 
 # Create your views here.
+clicks = clicks.prefetch_related('user')
+
+paginator = Paginator(clicks, 20)
+        page = request.GET.get('page')
+    try:
+        clicks = paginator.page(page)
+    except PageNotAnInteger:
+        # If page number is not an integer, gotot first page
+        clicks = paginator.page(1)
+    except EmptyPage:
+        # If page number is out of range, give last page
+        clicks = paginator.page(paginator.num_pages)
+    return render(request,
+                  'urly/clicks.html'),
+                  {'clicks': clicks}
