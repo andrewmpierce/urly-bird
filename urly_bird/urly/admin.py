38ed9f197c6e06568769c95e61930cab689d4290
Kathleen Rauh <krauh2010@gmail.com>
2015-10-16 11:19:06 -0400

    Early versions of files.

--- urly_bird/urly/admin.py
+++ urly_bird/urly/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+class BirdAdmin(admin.ModelAdmin):
+    pass
+
+
+class ClickAdmin(admin.ModelAdmin):
+    pass
+
+
+class AccessedAdmin(modelAdmin):
+    pass
--- urly_bird/urly/models.py
+++ urly_bird/urly/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Url_user(models.Model):
+    username = models.CharField(max_length=60)
+
+
+class Click(models.Model):
+    click_id = models.ForeignKey(URl_user)
+    title = models.CharField(max_length=40)
+    desc = models.CharField(max_length=160)
+    orig_timestamp = models.DateTimeField(auto_now_add=True)
+
+class Accessed(models.Model):
+    accessed_id = models.ForeignKey(Click)
+    new_timestamp = models.DateTimeStamp(auto_now=True)
+    click_count = models.SmallIntegerField()
