d7f84cbfa39e37e28347925a79252f3201505703
Kathleen Rauh <krauh2010@gmail.com>
2015-10-17 11:36:18 -0400

    Solving merge conflicts.

--- urly_bird/urly/urls.py
+++ urly_bird/urly/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-        url(r'^$', views.new_url, name='new_url')
-        url(r'^stats/(?P<click_short>\w+)$', views.stats_detail, name='stats_detail'),
-        url(r'^(?P<click_short>\w+)$', views.short, name='redirect_shorts'),
+    url(r'^$', views.new_url, name='new_url'),
+    url(r'^stats/(?P<click_short>\w+)$', views.stats_detail, name='stats_detail'),
+    url(r'^(?P<click_short>\w+)$', views.short, name='redirect_shorts'),
 ]
