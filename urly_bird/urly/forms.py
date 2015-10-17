fe49f1c8b8d370ef8569fe9fd434440d3754dac0
Kathleen Rauh <krauh2010@gmail.com>
2015-10-17 00:04:06 -0400

    Working on views and urls.

--- urly_bird/urly/forms.py
+++ urly_bird/urly/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class UrlForm(forms.Form):   # http://pythoncentral.io/how-to-use-python-django-forms/
     orig = forms.CharField(max_length=256)
-    created_at = forms.DateTimeField()
+    accessed = forms.IntegerField()
--- urly_bird/urly/views.py
+++ urly_bird/urly/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views import generic
-from django.core import Paginator, EmptyPage, PageNotAnInteger
+# from django.core import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
-class IndexView(generic.Listview):
+class IndexView(generic.ListView):
     template_name = 'template/click_detail.html'
     context_object_name = 'clicks'
     paginate_by=6
@@ -13,28 +14,33 @@ class IndexView(generic.Listview):
         return Click.objects.order_by('-timestamp')
 
 
-def author_clicks(request):     # can probably just get these with a sort
-    pass
+def new_url(request):
+     return HttpResponse("Thank God something shows up!!!!!")
 
+
+#
+# def author_clicks(request):     # can probably just get these with a sort
+#     pass
+#
 #
 # def all_clicks(request):
 #     clicks = Clicks.object.all()
 #     clicks_strings = [str(clicks) for click in clicks]
 #     return HttpResponse('<br>'.join(clicks_strings))
-
-
-clicks = clicks.prefetch_related('user')
-
-paginator = Paginator(clicks, 20)
-        page = request.GET.get('page')
-    try:
-        clicks = paginator.page(page)
-    except PageNotAnInteger:
-        # If page number is not an integer, gotot first page
-        clicks = paginator.page(1)
-    except EmptyPage:
-        # If page number is out of range, give last page
-        clicks = paginator.page(paginator.num_pages)
-    return render(request,
-                  'urly/clicks.html'),
-                  {'clicks': clicks}
+#
+#
+# clicks = clicks.prefetch_related('user')
+#
+# paginator = Paginator(clicks, 20)
+#         page = request.GET.get('page')
+#     try:
+#         clicks = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page number is not an integer, gotot first page
+#         clicks = paginator.page(1)
+#     except EmptyPage:
+#         # If page number is out of range, give last page
+#         clicks = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'urly/clicks.html'),
+#                   {'clicks': clicks}
