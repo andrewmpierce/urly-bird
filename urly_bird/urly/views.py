6eab9576ddf5321bb3a8b7d5336993ee95bafac1
apierce1992 <apierce1992@gmail.com>
2015-10-17 15:54:21 -0400

    views

--- urly_bird/urly/views.py
+++ urly_bird/urly/views.py
@@ -19,11 +19,33 @@ def short(request, click_short):
 
 def stats_detail(request, click_short):
     click = Click.objects.get(short=click_short)
-    stats = []
+    stats_list = []
     for stat in click.stats_set.all():
-        stats.append({'reader':stat.reader,
+        stats_list.append({'reader':stat.reader,
                         'timestamp': stat.timestamp})
-                        
     return render(request, 'urly/stats_detail.html',
         {'click': click,
-        'stats':stats})
+        #'stats': stats,
+        'stats_list':stats_list})
+
+def stats_chart(request, click_pk):
+    day_counts = {}
+    stats = Stats.objects.filter(click=click_pk).filter(timestamp__gte=timezone.now() - timedelta(days=30))
+    for stat in stats:
+        if stat.timestamp.day not in day_counts:
+            day_counts[stat.timestamp.day] = 1
+        else:
+            day_counts[stat.timestamp.day] +=1
+    day_counts = sorted(day_counts.items(), key=lambda x: x[0])
+    x_vals = []
+    y_vals = []
+    for x in day_counts:
+        x_vals.append(x[0])
+        y_vals.append(x[1])
+    fig = plt.figure()
+    #fig.patch.set_alpha(0)
+    plt.plot(x_vals,y_vals)
+    canvas = FigureCanvas(fig)
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
