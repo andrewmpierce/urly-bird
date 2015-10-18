c0279bf1817bb66d5102dddf638b3fc57a5de719
apierce1992 <apierce1992@gmail.com>
2015-10-18 12:01:19 -0400

    worked on list views

--- urly_bird/urly/urls.py
+++ urly_bird/urly/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import ListView
+from .models import Click
 
 
 
 urlpatterns = [
         url(r'^stats/(?P<click_short>\w+)$', views.stats_detail, name='stats_detail'),
-        #url(r'^list$', views.list_clicks, name='list_clicks'),
+        url(r'^list$', ListView.as_view(model=Click, paginate_by=25), name='click_list'),
         url(r'^(?P<click_short>\w+)$', views.short, name='redirect_shorts'),
         url(r'^clicks.png/(?P<click_pk>\d*)?$', views.stats_chart, name="stats_chart"),
         url(r'^user/(?P<username>\w+)$', views.user_table, name='user_table'),
+        url(r'^$', views.index, name='index')
 
 ]
--- urly_bird/urly/views.py
+++ urly_bird/urly/views.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
+from hashids import Hashids
+import random
+from django.views.generic import ListView
 
 
 
@@ -39,6 +42,23 @@ def stats_detail(request, click_short):
         #'stats': stats,
         'stats_list':stats_list})
 
+def index(request):
+    hashids = Hashids(salt="thisissalt")
+    if request.method == 'POST':
+        #if request.user.is_authenticated():
+        #author= request.user.username
+        author = User.objects.get(username='diandra.sipes')
+        orig = request.POST['orig']
+        x = random.choice([x for x in range(500)])
+        new_click = Click(author=author, orig=orig,
+                          short = hashids.encode(x),
+                          title=orig, timestamp=datetime.now())
+        return render(request,
+                    'urly/index.html',
+                      {'submitted':True,
+                      'click':new_click })
+    return render(request, 'urly/index.html')
+
 
 
 def stats_chart(request, click_pk):
@@ -57,6 +77,7 @@ def stats_chart(request, click_pk):
     fig = plt.figure(figsize=(10,4))
     #fig.patch.set_alpha(0)
     plt.plot(x_vals,y_vals)
+    plt.xticks([1,5,10,15,20,25,30])
     plt.title("Popularity of Bookmark for the Previous Month")
     plt.xlabel("Day of the Month")
     plt.ylabel("Number of Clicks")
@@ -71,3 +92,23 @@ def user_table(request, username):
     return render(request, 'urly/user_table.html',
         {'clicks':clicks,
         'user': user})
+
+
+# def list_clicks(request):
+#     clicks = Click.objects.order_by('-timestamp').all()
+#     paginator = Paginator(clicks, 20)
+#     page = request.GET.get('page')
+#     try:
+#         clicks = paginator.page('page')
+#     except PageNotAnInteger:
+#         clicks = paginator.page(1)
+#     except EmptyPage:
+#          clicks = paginator.page(paginator.num_pages)
+#     return render_to_response('urly/list_clicks.html', {"clicks":clicks})
+
+
+class ClickListView(ListView):
+    model = Click      # shorthand for setting queryset = models.Car.objects.all()
+    template_name = 'urly/click_list.html'  # optional (the default is app_name/modelNameInLowerCase_list.html; which will look into your templates folder for that path and file)
+    context_object_name = "click_list"    #default is object_list as well as model's_verbose_name_list and/or model's_verbose_name_plural_list, if defined in the model's inner Meta class
+    paginate_by = 10  #and that's it !!
