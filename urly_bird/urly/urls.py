2eaaf3b438aa9939c46f6c60febcdc51436920b0
Kathleen Rauh <krauh2010@gmail.com>
2015-10-17 22:19:12 -0400

    More work on click detial.

--- urly_bird/urly/urls.py
+++ urly_bird/urly/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.new_url, name='new_url'),
+    url(r'^$', IndexView_as_view),
     url(r'^stats/(?P<click_short>\w+)$', views.stats_detail, name='stats_detail'),
     url(r'^(?P<click_short>\w+)$', views.short, name='redirect_shorts'),
 ]
